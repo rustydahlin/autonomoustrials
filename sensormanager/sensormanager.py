74a3beea5fcfe555e2eb8e39d87979714c01a216
Midpipps <midpipps@gmail.com>
2019-07-26 01:02:58 -0500

    Adding the initial color sensor code for the ts34725

--- sensormanager/sensormanager.py
+++ sensormanager/sensormanager.py
@@ -1,6 +1,14 @@
 '''
 SensorManager keeps all the sensor data updated without overpolling any 1 sensor
 '''
+
+import board
+import busio
+import adafruit_tcs34725
+
+I2C = busio.I2C(board.SCL, board.SDA)
+
+
 class Sensor(object):
     '''
     Base sensor class/interface
@@ -22,7 +30,8 @@ class Sensor(object):
         '''
         if isinstance(idval, str):
             self.__idval = idval
-        raise Exception("idval was of the incorrect type for sensor object")
+        else:
+            raise Exception("idval was of the incorrect type for sensor object")
 
     def pollsensor(self):
         raise NotImplementedError('pollsensor method has not been implemented yet')
@@ -37,6 +46,52 @@ class I2CSensor(Sensor):
     def __init__(self, sensoraddress, idval):
         self.sensoraddress = sensoraddress
         super().__init__(idval)
+
+    @property
+    def sensoraddress(self):
+        return self.__sensoraddress
+
+    @sensoraddress.setter
+    def sensoraddress(self, sensoraddress):
+        self.__sensoraddress = sensoraddress
+
+class ColorSensor(I2CSensor):
+    '''
+    TCS34725 color sensor class
+    Documentation https://buildmedia.readthedocs.org/media/pdf/adafruit-circuitpython-tcs34725/latest/adafruit-circuitpython-tcs34725.pdf
+    '''
+    def __init__(self, sensoraddress, idval, integration_time=200, gain=60):
+        self.__rgb = None
+        self.__temperature = None
+        self.__lux = None
+        self.__sensor = adafruit_tcs34725.TCS34725(I2C)
+        self.integration_time = integration_time
+        self.gain = gain
+        super().__init__(sensoraddress, idval)
+
+    @property
+    def integration_time(self):
+        return self.__sensor.integration_time
+
+    @integration_time.setter
+    def integration_time(self, integration_time):
+        self.__sensor.integration_time = integration_time
+
+    @property
+    def gain(self):
+        return self.__sensor.gain
+
+    @gain.setter
+    def gain(self, gain):
+        self.__sensor.gain = gain
+
+    def pollsensor(self):
+        self.__lux = self.__sensor.lux
+        self.__temperature = self.__sensor.color_temperature
+        self.__rgb = self.__sensor.color_rgb_bytes
+
+    def getdatadict(self):
+        return {'rgb':self.__rgb, 'lux':self.__lux, 'temperature':self.__temperature}
 
 
 class SensorManager(object):
